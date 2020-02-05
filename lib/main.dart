dbf2653e8e5b3deb8a06227878eaccf7134b31b3
SupposedlySam <supposedlysam@gmail.com>
2020-02-05 13:55:41 -0700

    Start from clean slate

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class FlutterResume extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeView(title: 'Flutter Demo Home Page'),
+      home: HomeView(),
     );
   }
 }
--- lib/presentation/views/home_view.dart
+++ lib/presentation/views/home_view.dart
@@ -1,48 +1,8 @@
 import 'package:flutter/material.dart';
 
-class HomeView extends StatefulWidget {
-  HomeView({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends State<HomeView> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+    return Scaffold(body: Container());
   }
 }
