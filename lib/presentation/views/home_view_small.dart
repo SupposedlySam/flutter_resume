0f111d0c41362ab7e5f5d064184882a2922e7e6b
Jonah Walker <supposedlysam@gmail.com>
2021-04-19 11:32:36 -0400

    Show user more info

--- lib/presentation/views/home_view_small.dart
+++ lib/presentation/views/home_view_small.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class HomeViewSmall extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(child: Text('Intentionally Left Blank. Fork this project and complete the mobile view!'));
   }
 }
